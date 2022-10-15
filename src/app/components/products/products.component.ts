5e34907ba8479985ef50605a57fdcdeb6df09813
lyeslehara1996 <lyeslehara88@gmail.com>
2022-10-15 22:21:03 +0200

    angular project with State Managment

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { Product } from 'src/app/Models/product';
+import { ProductsService } from 'src/app/Services/products.service';
+import { AppDataState, DataStateEnum } from 'src/app/State/productState';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+
+export class ProductsComponent implements OnInit {
+
+  products$ ?:Observable<AppDataState< Product[]>> | null=null;
+
+  readonly DataStateEnum = DataStateEnum;
+
+  constructor(private productservice:ProductsService) { }
+
+  ngOnInit(): void {
+  }
+
+  onGetAllProducts(){
+    this.products$=
+    this.productservice.getAllProduct().pipe(
+      map(
+        data=>{
+          console.log(data);
+       return ({dataState:DataStateEnum.LOADED,data:data}) 
+        }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(error=> of({dataState:DataStateEnum.ERROR,errorMessage:error.Message})),
+    );
+  }
+
+
+  onGetSelectProduct(){
+
+      this.products$=
+    this.productservice.getSelectProduct().pipe(
+      map(
+        data=>{
+          console.log(data);
+       return ({dataState:DataStateEnum.LOADED, data:data}) 
+        }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(error=> of({dataState:DataStateEnum.ERROR,errorMessage:error.Message})),
+    );
+  }
+
+
+  onGetAvailableProduct(){
+          this.products$=
+    this.productservice.getAvailableProduct().pipe(
+      map(
+        data=>{
+          console.log(data);
+       return ({dataState:DataStateEnum.LOADED, data:data}) 
+        }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(error=> of({dataState:DataStateEnum.ERROR,errorMessage:error.Message})),
+    );
+ }
+
+onSearch(dataForm:any){
+ this.products$=
+    this.productservice.getSearchProduct(dataForm.keyWord).pipe(
+      map(
+        data=>{
+          console.log(data);
+       return ({dataState:DataStateEnum.LOADED, data:data}) 
+        }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(error=> of({dataState:DataStateEnum.ERROR,errorMessage:error.Message})),
+    );
+}
+
+onSelect(p:Product){
+  this.productservice.SelectProduct(p).subscribe(data=>{
+    p.selected=data.selected;
+  })
+}
+
+onDelete(p : Product){
+  let v = confirm("vous voulez supprimer cet element ");
+  if(v===true)
+  this.productservice.deleteProduct(p).subscribe(data=>{
+    this.onGetAllProducts()
+  })
+}
+  }
+
